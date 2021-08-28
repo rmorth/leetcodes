cc36347d0b4d72e71523a35c6f9e6504d2195d10
SirMorthwyl <rodrigosantosmorgado@gmail.com>
2021-08-28 15:19:33 +0100

    Added problem longest-common-prefix; Implemented solution for longest-common-prefix

---
+++ longest-common-prefix/python/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = strs[0]
+
+        for s in strs:
+            while not s.startswith(prefix):
+                prefix = prefix[:-1]
+                if prefix == "":
+                    return ""
+
+        return prefix
