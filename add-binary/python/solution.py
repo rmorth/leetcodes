f965c286d927469ec2984ff0e5a42c5b9bc6e087
SirMorthwyl <rodrigosantosmorgado@gmail.com>
2021-09-06 15:44:51 +0100

    Implemented solution for add-binary

--- add-binary/python/solution.py
+++ add-binary/python/solution.py
@@ -1,3 +1,28 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        pass
+        a, b = list(a), list(b)
+        rem, res = 0, ""
+
+        while a or b or rem:
+            rem += int(a.pop()) if a else 0
+            rem += int(b.pop()) if b else 0
+
+            res = str(rem % 2) + res
+            rem //= 2
+        return res
+
+
+print(Solution().addBinary("1010", "1011"))
+
+
+"""
+Cheeky but not fun way of solving it:
+
+a = "0b" + a
+b = "0b" + b
+s = int(a, base=0) + int(b, base=0)
+return bin(s).replace("0b", "")
+
+(Leads to stupidly good results of course)
+
+"""
